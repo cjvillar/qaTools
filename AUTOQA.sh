0445d34b79de84a71c0eadef7c18a9f9bd7b66c3
Chris Villarreal <cjvillar@ucsc.edu>
2017-02-21 09:09:19 -0800

    added pushq.sh to AUTOQA.sh

--- AUTOQA.sh
+++ AUTOQA.sh
@@ -17,7 +17,8 @@ fi
 echo "Enter directory name"
 read dirname
 
-exists=$(hgsql -Ne "SHOW DATABASES LIKE '$dirname'")
+exists=$(hgsql -h mysqlbeta qapushq -Ne "show tables like '$dirname'")
+
  if [ "$exists" == "" ]; then echo "'$dirname'  doesn't exist."; exit 0; fi
 
 if [ ! -d "../$dirname" ]
@@ -33,14 +34,19 @@ else
 fi
 
 
-# Within this directory do SQL query for the tables listed in the directories name. I need to find a good way to extract all the tables needed when pushq gets retired  
+# Within this directory do SQL query for the tables listed in the directories name. As of now the tables retrieved from the pushQ.  
+#hgsql -Ne "show tables" $dirname > ../$dirname/rawTablelist.txt 
+#cat ../$dirname/rawTablelist.txt | if grep "ERROR" ; then echo "mySQL error check $dirname for mispelling"; 
+# exit 0; fi 
 
+prepname="$(tr '[:lower:]' '[:upper:]' <<< ${dirname:0:1})${dirname:1}"
 
-hgsql -Ne "show tables" $dirname > ../$dirname/rawTablelist.txt 
-
-
-cat ../$dirname/rawTablelist.txt | if grep "ERROR" ; then echo "mySQL error check $dirname for mispelling"; 
- exit 0; fi 
+hgsql -h mysqlbeta qapushq -Ne "SELECT tbls FROM $dirname WHERE dbs='$dirname'" > ./$dirname/pushQtablesRaw;
+awk -v RS=" " '{print}' ./$dirname/pushQtablesRaw > ./$dirname/pushQtablesClean ; cat ./$dirname/pushQtablesClean | sort > ./$dirname/pushQtablesCleanSorted;
+cat ./$dirname/pushQtablesCleanSorted | grep -v -e 'trackDb*' -e 'hgFindSpec*' > ./$dirname/tables${prepname};
+sed -i '/^$/d' ./$dirname/tables${prepname} ; cat ./$dirname/tables${prepname} | wc -l > count; mv count ./$dirname/$(head -1 count).pushQtableCount ;
+cat ./$dirname/tables${prepname};
+echo “Paste this list into your track QA spreadsheet”;
 
 # Remove any hgFindSpec* and trackDb* files from rawTablelist.txt
 # cat rawTablelist.txt | grep -v 'hgFindSpec*\trackDb*' > ../$dirname/Tablelist.txt
